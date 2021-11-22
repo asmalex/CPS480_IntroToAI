abfa246942f355dc011942138ca40451d5c7ebb5
Alexander Redei <8584571+asmalex@users.noreply.github.com>
2021-11-22 11:02:00 -0500

    Added MountainCar

---
+++ MountainCar_Spaces.py
@@ -0,0 +1,26 @@
+import gym 
+import random 
+
+#Create our MountainCar Environment
+env = gym.make('MountainCar-v0') 
+env.reset() 
+
+def RandomAgent(): 
+    #each episode is a game that is run to its end 
+    
+    for epoch in range (10): 
+        #now we break up the frames 
+        for t in range (500): 
+            env.render() 
+            
+            action = env.action_space.sample()
+
+            #the next state, rewards, done flag, and info are all returned 
+            next_state, reward, done, info = env.step(action) 
+            print(t, next_state, reward, done, info, action) 
+            
+            if done: 
+                env.reset() 
+                break
+
+RandomAgent()
